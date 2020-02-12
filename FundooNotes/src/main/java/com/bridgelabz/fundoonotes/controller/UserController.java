c8d8532781954f4576e2f0b97988440206dd82c0
brijesh-biJJi <brijeshkanchan7@gmail.com>
2020-02-12 19:01:16 +0530

    [Brijesh] Add. Implemented UserDto class

--- FundooNotes/src/main/java/com/bridgelabz/fundoonotes/controller/UserController.java
+++ FundooNotes/src/main/java/com/bridgelabz/fundoonotes/controller/UserController.java
@@ -1,5 +1,18 @@
 package com.bridgelabz.fundoonotes.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bridgelabz.fundoonotes.dto.UserDto;
+
+@RestController
 public class UserController {
 
+	@PostMapping("/user/registration")
+	public ResponseEntity<Response> register(@RequestBody UserDto info)
+	{
+	
+	}
 }
