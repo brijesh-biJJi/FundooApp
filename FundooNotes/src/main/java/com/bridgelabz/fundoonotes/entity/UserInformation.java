7198b000c4417f1084bc48fd79d225a3293a3c13
brijesh-biJJi <brijeshkanchan7@gmail.com>
2020-02-12 19:17:57 +0530

    [Brijesh] Add. Implemented UserInformation class

---
+++ FundooNotes/src/main/java/com/bridgelabz/fundoonotes/entity/UserInformation.java
@@ -0,0 +1,65 @@
+package com.bridgelabz.fundoonotes.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="userinfo")
+public class UserInformation {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	
+	private long userid;
+	private String name;
+	private String password;
+	private long phone;
+	private boolean isVerified;
+	private LocalDateTime dateTime;
+	public long getUserid() {
+		return userid;
+	}
+	public void setUserid(long userid) {
+		this.userid = userid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	public boolean isVerified() {
+		return isVerified;
+	}
+	public void setVerified(boolean isVerified) {
+		this.isVerified = isVerified;
+	}
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+	public void setDateTime(LocalDateTime dateTime) {
+		this.dateTime = dateTime;
+	}
+	
+	
+}
---
+++ FundooNotes/src/main/java/com/bridgelabz/fundoonotes/service/Services.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.fundoonotes.service;
+
+import com.bridgelabz.fundoonotes.dto.UserDto;
+
+public interface Services {
+
+	boolean register(UserDto info);
+	
+	
+}
