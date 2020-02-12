96bff21efede69ba5d93f13565354742d6f2c098
brijesh-biJJi <brijeshkanchan7@gmail.com>
2020-02-12 19:23:11 +0530

    [Brijesh] Add. Implemented LoginInformation class

--- FundooNotes/src/main/java/com/bridgelabz/fundoonotes/service/Services.java
+++ FundooNotes/src/main/java/com/bridgelabz/fundoonotes/service/Services.java
@@ -1,10 +1,11 @@
 package com.bridgelabz.fundoonotes.service;
 
 import com.bridgelabz.fundoonotes.dto.UserDto;
+import com.bridgelabz.fundoonotes.entity.UserInformation;
 
 public interface Services {
 
 	boolean register(UserDto info);
 	
-	
+	UserInformation login(LoginInformation info)
 }
