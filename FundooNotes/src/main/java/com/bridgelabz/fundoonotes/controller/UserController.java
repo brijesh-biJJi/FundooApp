a53313a2d62cbdb5384584e3790c494cb2d3c0de
brijesh-biJJi <brijeshkanchan7@gmail.com>
2020-02-13 11:00:54 +0530

    [Brijesh] Add. Implemented UserRepository interface

--- FundooNotes/src/main/java/com/bridgelabz/fundoonotes/controller/UserController.java
+++ FundooNotes/src/main/java/com/bridgelabz/fundoonotes/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.fundoonotes.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,15 +9,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.bridgelabz.fundoonotes.dto.UserDto;
 import com.bridgelabz.fundoonotes.response.Response;
+import com.bridgelabz.fundoonotes.service.Services;
 
 @RestController
 public class UserController {
 
-	@PostMapping("/user/registration")
+	@Autowired
+	private Services service;
+	@PostMapping("/user/register")
 	public ResponseEntity<Response> register(@RequestBody UserDto info)
 	{
+		boolean result = service.register(info);
+		if(result) {
+			return ResponseEntity.status(HttpStatus.CREATED).body(new Response("registration successfull", 200, info));
+		}
+				return ResponseEntity.status(HttpStatus.ALREADY_REPORTED).body(new Response("user already ", 400, info));
 		
-		return null;
+		
+		
+		//return ResponseEntity.status(HttpStatus.OK).body(new Response("Hello ", 400, info));
 	
 	}
 }
--- FundooNotes/src/main/java/com/bridgelabz/fundoonotes/service/Services.java
+++ FundooNotes/src/main/java/com/bridgelabz/fundoonotes/service/Services.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.fundoonotes.service;
 
+import com.bridgelabz.fundoonotes.dto.LoginInformation;
 import com.bridgelabz.fundoonotes.dto.UserDto;
 import com.bridgelabz.fundoonotes.entity.UserInformation;
 
@@ -7,5 +8,7 @@ public interface Services {
 
 	boolean register(UserDto info);
 	
-	UserInformation login(LoginInformation info)
+	UserInformation login(LoginInformation info);
+	
+	
 }
