1b455cb6f5f9105fa1124adc9875a139ea6ae523
brijesh-biJJi <brijeshkanchan7@gmail.com>
2020-02-27 15:12:25 +0530

    [Brijesh] Add. Implemented Profile class

---
+++ FundooNotes/src/main/java/com/bridgelabz/fundoonotes/config/RedisConfig.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.fundoonotes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+public class RedisConfig {
+
+	@Bean
+	JedisConnectionFactory jedisConnectionFactory() {
+		return new JedisConnectionFactory();
+	}
+
+	@Bean
+	RedisTemplate<String, Object> redisTemplate() {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+		redisTemplate.setConnectionFactory(jedisConnectionFactory());
+		return redisTemplate;
+	}
+}
---
+++ FundooNotes/src/main/java/com/bridgelabz/fundoonotes/entity/Profile.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.fundoonotes.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Profile {
+
+}
